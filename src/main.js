29fdb0cebe32933dc2c3e909ab982fec1ea33c2f
Ricardo <958990024@qq.com>
2019-10-03 19:57:03 +0800

    complete some pages

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './routers'
+import store from './store'
 
 // eslint-disable-next-line no-new
 new Vue({
   el: '#app',
   render: h => h(App),
-  router
+  router,
+  store
 })
--- src/pages/Msite/Msite.vue
+++ src/pages/Msite/Msite.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="msite">
     <!--首页头部-->
-    <HeaderTop title="昌平区北七家宏福科技园(337省道北)">
+    <HeaderTop :title="address.name">
       <span class="header_search" slot="left">
         <i class="iconfont icon-sousuo"></i>
       </span>
@@ -11,112 +11,26 @@
     </HeaderTop>
     <!--首页导航-->
     <nav class="msite_nav">
-      <div class="swiper-container">
+      <div class="swiper-container" v-if="categorys.length">
         <div class="swiper-wrapper">
-          <div class="swiper-slide">
-            <a href="javascript:" class="link_to_food">
+          <div class="swiper-slide" v-for="(categorys, index) in categorysArr" :key="index">
+            <a
+              href="javascript:"
+              class="link_to_food"
+              v-for="(category, index) in categorys"
+              :key="index"
+            >
               <div class="food_container">
-                <img src="./images/nav/1.jpg" />
+                <img :src="baseImageUrl + category.image_url" />
               </div>
-              <span>甜品饮品</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/2.jpg" />
-              </div>
-              <span>商超便利</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/3.jpg" />
-              </div>
-              <span>美食</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/4.jpg" />
-              </div>
-              <span>简餐</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/5.jpg" />
-              </div>
-              <span>新店特惠</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/6.jpg" />
-              </div>
-              <span>准时达</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/7.jpg" />
-              </div>
-              <span>预订早餐</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/8.jpg" />
-              </div>
-              <span>土豪推荐</span>
-            </a>
-          </div>
-          <div class="swiper-slide">
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/9.jpg" />
-              </div>
-              <span>甜品饮品</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/10.jpg" />
-              </div>
-              <span>商超便利</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/11.jpg" />
-              </div>
-              <span>美食</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/12.jpg" />
-              </div>
-              <span>简餐</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/13.jpg" />
-              </div>
-              <span>新店特惠</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/14.jpg" />
-              </div>
-              <span>准时达</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/1.jpg" />
-              </div>
-              <span>预订早餐</span>
-            </a>
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/nav/2.jpg" />
-              </div>
-              <span>土豪推荐</span>
+              <span>{{category.title}}</span>
             </a>
           </div>
         </div>
         <!-- Add Pagination -->
         <div class="swiper-pagination"></div>
       </div>
+      <img src="./images/msite_back.svg" alt="back" v-else>
     </nav>
     <!--首页附近商家-->
     <div class="msite_shop_list">
@@ -124,12 +38,14 @@
         <i class="iconfont icon-xuanxiang"></i>
         <span class="shop_header_title">附近商家</span>
       </div>
-      <ShopList/>
+      <ShopList />
     </div>
   </section>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 import Swiper from 'swiper/js/swiper.min.js'
 import 'swiper/css/swiper.min.css'
 
@@ -137,17 +53,79 @@ import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
 import ShopList from '../../components/ShopList/ShopList.vue'
 
 export default {
-  mounted () {
-    // 创建一个 Swiper 实例对象，来实现轮播
-    // eslint-disable-next-line no-new
-    new Swiper('.swiper-container', {
-      loop: true,
-      autoplay: true,
-      pagination: {
-        el: '.swiper-pagination'
-      }
-    })
+  data () {
+    return {
+      baseImageUrl: 'https://fuss10.elemecdn.com'
+    }
   },
+
+  mounted () {
+    this.$store.dispatch('getCategorys')
+    this.$store.dispatch('getShops')
+  },
+
+  watch: {
+    categorys (value) {
+      /*
+      // 使用setimeout可以实现效果，但不是最好
+      setTimeout(() => {
+        // 创建一个 Swiper 实例对象，来实现轮播
+        // eslint-disable-next-line no-new
+        new Swiper('.swiper-container', {
+          loop: true,
+          autoplay: true,
+          pagination: {
+            el: '.swiper-pagination'
+          }
+        })
+      }, 100) */
+
+      // 界面更新就立即创建 Swiper 对象
+      this.$nextTick(() => {
+        // 一旦完成界面更新，立即调用(此条语句要写在数据更新之后)
+        // 创建一个 Swiper 实例对象，来实现轮播
+        // eslint-disable-next-line no-new
+        new Swiper('.swiper-container', {
+          loop: true,
+          autoplay: true,
+          pagination: {
+            el: '.swiper-pagination'
+          }
+        })
+      })
+    }
+  },
+
+  computed: {
+    ...mapState(['address', 'categorys']),
+
+    // 根据 categorys 一维数组生成一个二维数组
+    // 小数组中的元素个数最大是8
+    categorysArr () {
+      const { categorys } = this
+      // 生成一个空的二维数组
+      const arr = []
+      let minArr = []
+      // 遍历 categorys
+      categorys.forEach(c => {
+        // 如果当前小数组已经满了，创建一个新的
+        if (minArr.length === 8) {
+          minArr = []
+        }
+
+        // 如果 minArr 为空，将小数组保存到大数组中
+        if (minArr.length === 0) {
+          arr.push(minArr)
+        }
+
+        // 将当前分类保存到小数组中
+        minArr.push(c)
+      })
+
+      return arr
+    }
+  },
+
   components: {
     HeaderTop,
     ShopList
@@ -156,96 +134,139 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  @import '../../common/stylus/mixins.styl'
-  .msite  //首页
-    width 100%
-    .header
-      background-color #02a774
-      position fixed
-      z-index 100
-      left 0
-      top 0
-      width 100%
-      height 45px
-      .header_search
-        position absolute
-        left 15px
-        top 50%
-        transform translateY(-50%)
-        width 10%
-        height 50%
-        .icon-sousuo
-          font-size 25px
-          color #fff
-      .header_title
-        position absolute
-        top 50%
-        left 50%
-        transform translate(-50%, -50%)
-        width 50%
-        color #fff
-        text-align center
-        .header_title_text
-          font-size 20px
-          color #fff
-          display block
-      .header_login
-        font-size 14px
-        color #fff
-        position absolute
-        right 15px
-        top 50%
-        transform translateY(-50%)
-        .header_login_text
-          color #fff
-    .msite_nav
-      bottom-border-1px(#e4e4e4)
-      margin-top 45px
-      height 200px
-      background #fff
-      .swiper-container
-        width 100%
-        height 100%
-        .swiper-wrapper
-          width 100%
-          height 100%
-          .swiper-slide
-            display flex
-            justify-content center
-            align-items flex-start
-            flex-wrap wrap
-            .link_to_food
-              width 25%
-              .food_container
-                display block
-                width 100%
-                text-align center
-                padding-bottom 10px
-                font-size 0
-                img
-                  display inline-block
-                  width 50px
-                  height 50px
-              span
-                display block
-                width 100%
-                text-align center
-                font-size 13px
-                color #666
-        .swiper-pagination
-          >span.swiper-pagination-bullet-active
-            background #02a774
-    .msite_shop_list
-      top-border-1px(#e4e4e4)
-      margin-top 10px
-      background #fff
-      .shop_header
-        padding 10px 10px 0
-        .shop_icon
-          margin-left 5px
-          color #999
-        .shop_header_title
-          color #999
-          font-size 14px
-          line-height 20px
+@import '../../common/stylus/mixins.styl';
+
+.msite { // 首页
+  width: 100%;
+
+  .header {
+    background-color: #02a774;
+    position: fixed;
+    z-index: 100;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 45px;
+
+    .header_search {
+      position: absolute;
+      left: 15px;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 10%;
+      height: 50%;
+
+      .icon-sousuo {
+        font-size: 25px;
+        color: #fff;
+      }
+    }
+
+    .header_title {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 50%;
+      color: #fff;
+      text-align: center;
+
+      .header_title_text {
+        font-size: 20px;
+        color: #fff;
+        display: block;
+      }
+    }
+
+    .header_login {
+      font-size: 14px;
+      color: #fff;
+      position: absolute;
+      right: 15px;
+      top: 50%;
+      transform: translateY(-50%);
+
+      .header_login_text {
+        color: #fff;
+      }
+    }
+  }
+
+  .msite_nav {
+    bottom-border-1px(#e4e4e4);
+    margin-top: 45px;
+    height: 200px;
+    background: #fff;
+
+    .swiper-container {
+      width: 100%;
+      height: 100%;
+
+      .swiper-wrapper {
+        width: 100%;
+        height: 100%;
+
+        .swiper-slide {
+          display: flex;
+          justify-content: center;
+          align-items: flex-start;
+          flex-wrap: wrap;
+
+          .link_to_food {
+            width: 25%;
+
+            .food_container {
+              display: block;
+              width: 100%;
+              text-align: center;
+              padding-bottom: 10px;
+              font-size: 0;
+
+              img {
+                display: inline-block;
+                width: 50px;
+                height: 50px;
+              }
+            }
+
+            span {
+              display: block;
+              width: 100%;
+              text-align: center;
+              font-size: 13px;
+              color: #666;
+            }
+          }
+        }
+      }
+
+      .swiper-pagination {
+        >span.swiper-pagination-bullet-active {
+          background: #02a774;
+        }
+      }
+    }
+  }
+
+  .msite_shop_list {
+    top-border-1px(#e4e4e4);
+    margin-top: 10px;
+    background: #fff;
+
+    .shop_header {
+      padding: 10px 10px 0;
+
+      .shop_icon {
+        margin-left: 5px;
+        color: #999;
+      }
+
+      .shop_header_title {
+        color: #999;
+        font-size: 14px;
+        line-height: 20px;
+      }
+    }
+  }
+}
 </style>
